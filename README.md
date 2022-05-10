6fefd2adc54368cc060b0603f109701dfa1d36f8
Allen Chan <allenchan1010@outlook.com>
2022-05-10 22:36:29 +0800

    Add options implementation and update version to 0.5.0

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # fries
+
 Some useful functions base on C++11. For now, only include header files.
 
 ## futures
 
 ### future
-Implement future/then functionality. For sample code, check [future_test.cpp](https://github.com/4llenchan/fries/blob/master/tests/futures/future_test.cpp). 
- 
+
+Implement future/then functionality. For sample code,
+check [future_test.cpp](https://github.com/4llenchan/fries/blob/master/tests/futures/future_test.cpp).
+
 ## system
 
 ### switcher
-Implement a switcher for all types, including std::string. For sample code, check [switcher_test.cpp](https://github.com/4llenchan/fries/blob/master/tests/system/switcher_test.cpp).
 
+Implement a switcher for all types, including std::string. For sample code,
+check [switcher_test.cpp](https://github.com/4llenchan/fries/blob/master/tests/system/switcher_test.cpp).
+
+### options
+
+Implement a template for mask style enum types, to enable type safety manipulations. For sample code,
+check [options_test.cpp](https://github.com/4llenchan/fries/blob/master/tests/system/options_test.cpp).
 
 ## TODO
+
 - [ ] comments on switcher.cpp
---
+++ fries/system/options.hpp
@@ -0,0 +1,66 @@
+//
+// Created by AllenChan on 2022/5/10.
+//
+
+#ifndef FRIES_SYSTEM_OPTIONS_HPP_
+#define FRIES_SYSTEM_OPTIONS_HPP_
+
+#include <cstdint>
+#include <type_traits>
+#include <utility>
+
+namespace fries {
+namespace system {
+template <typename OptionType, typename Enable = void>
+class options {};
+
+/// \brief Template class to enable options for an enum.
+template <typename option_type>
+class options<option_type, typename std::enable_if<std::is_enum<option_type>::value>::type> {
+public:
+    explicit options(option_type option) : options_(cast_(option)) {}
+
+    options(const std::initializer_list<option_type>& options) : options_(0) {
+        for (auto option : options) {
+            options_ |= cast_(option);
+        }
+    }
+
+    options& operator|(const options<option_type>& other) {
+        if (this != &other) {
+            options_ |= other.options_;
+        }
+        return *this;
+    }
+
+    options& operator|=(const options<option_type>& other) {
+        if (this != &other) {
+            options_ |= other.options_;
+        }
+        return *this;
+    }
+
+    options& operator|(option_type option) {
+        options_ |= cast_(option);
+        return *this;
+    }
+
+    options& operator|=(option_type option) {
+        options_ |= cast_(option);
+        return *this;
+    }
+
+    inline bool has(option_type option) const { return (options_ & cast_(option)) > 0; }
+
+    inline void exclude(option_type option) { options_ &= ~cast_(option); }
+
+private:
+    using value_type = uint64_t;
+    static inline value_type cast_(option_type option) { return static_cast<value_type>(option); }
+
+    value_type options_;
+};
+}  // namespace system
+}  // namespace fries
+
+#endif  // FRIES_SYSTEM_OPTIONS_HPP_
